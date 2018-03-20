35ee7c7272cd628faf9f1f3982d0a450e2bcfb7a
James Matchett <30292929+JamesMatchett@users.noreply.github.com>
2018-03-20 12:18:09 +0000

    Add files via upload

---
+++ FluxSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluxSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> UnsortedList = new List<int>(){5,4,2,4,2,2};
+            List<int> sortedList = FluxSort(UnsortedList);
+            foreach (int q in sortedList)
+            {
+                Console.WriteLine(sortedList);
+
+            }
+            Console.ReadKey();
+        }
+
+        static List<int> FluxSort(List<int> UnsortedList)
+        {
+            List<int> SortedList = new List<int>();
+            
+            while (UnsortedList.Count != 0)
+            {
+                int Lowest = UnsortedList.Min();
+                SortedList.Add(Lowest);
+                UnsortedList.Remove(Lowest);
+                
+            }
+            return SortedList;
+        }
+    }
+}
